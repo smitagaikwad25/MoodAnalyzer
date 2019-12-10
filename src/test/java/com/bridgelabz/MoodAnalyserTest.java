919d5a515a15dec53b49bafacbfdb2c1bd3df0fc
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-10 18:13:32 +0530

    Refactor : TC4.3 remove comments

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -6,61 +6,61 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 
-//
-//    @Test
-//    public void givenMessage_WhenSad_Should_ReturnSad() throws MoodAnalyserException {
-//        MoodAnalyser analyzer = new MoodAnalyser("this Sad is message");
-//        String mood = analyzer.analyseMood();
-//        Assert.assertEquals("Sad", mood);
-//    }
-//
-//    @Test
-//    public void givenMessage_WhenAny_ShouldReturnHappy() throws MoodAnalyserException {
-//        MoodAnalyser analyzer = new MoodAnalyser("I am in any mood");
-//        String mood = analyzer.analyseMood();
-//        Assert.assertEquals("Happy", mood);
-//
-//    }
-//
-//    @Test
-//    public void givenMessage_WhenNull_ShouldReturnHappy() throws MoodAnalyserException {
-//        MoodAnalyser analyzer = new MoodAnalyser("NULL");
-//        String mood = analyzer.analyseMood();
-//        Assert.assertEquals("Happy", mood);
-//    }
-//
-//    @Test
-//    public void givenMessage_WhenNull_ShouldReturnMoodAnalyserException() {
-//        MoodAnalyser analyzer = new MoodAnalyser(null);
-//        try {
-//            analyzer.analyseMood();
-//        } catch (MoodAnalyserException e) {
-//            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
-//        }
-//    }
-//
-//    @Test
-//    public void givenMessage_WhenEmpty_ShouldReturnMoodAnalyserException() {
-//        MoodAnalyser analyser = new MoodAnalyser("");
-//        try {
-//            analyser.analyseMood();
-//        } catch (MoodAnalyserException e) {
-//            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
-//        }
-//    }
-//
-//    @Test
-//    public void givenMoodAnalyserClass_whenProper_ShouldReturnObject() throws MoodAnalyserException {
-//        MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser();
-//        Assert.assertEquals(new MoodAnalyser("I am in happy mood"), moodAnalyser);
-//    }
+
+    @Test
+    public void givenMessage_WhenSad_Should_ReturnSad() throws MoodAnalyserException {
+        MoodAnalyser analyzer = new MoodAnalyser("this Sad is message");
+        String mood = analyzer.analyseMood();
+        Assert.assertEquals("Sad", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenAny_ShouldReturnHappy() throws MoodAnalyserException {
+        MoodAnalyser analyzer = new MoodAnalyser("I am in any mood");
+        String mood = analyzer.analyseMood();
+        Assert.assertEquals("Happy", mood);
+
+    }
+
+    @Test
+    public void givenMessage_WhenNull_ShouldReturnHappy() throws MoodAnalyserException {
+        MoodAnalyser analyzer = new MoodAnalyser("NULL");
+        String mood = analyzer.analyseMood();
+        Assert.assertEquals("Happy", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenNull_ShouldReturnMoodAnalyserException() {
+        MoodAnalyser analyzer = new MoodAnalyser(null);
+        try {
+            analyzer.analyseMood();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+
+    @Test
+    public void givenMessage_WhenEmpty_ShouldReturnMoodAnalyserException() {
+        MoodAnalyser analyser = new MoodAnalyser("");
+        try {
+            analyser.analyseMood();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
+    }
+
+    @Test
+    public void givenMoodAnalyserClass_whenProper_ShouldReturnObject() throws MoodAnalyserException {
+        MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser();
+        Assert.assertEquals(new MoodAnalyser("I am in happy mood"), moodAnalyser);
+    }
 
     @Test
     public void givenMoodAnalyserClass_whenNotProper_ReturnMoodAnalyserException() {
         try {
             MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser();
         } catch (MoodAnalyserException e) {
-            Assert.assertEquals(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS,e.type);
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS, e.type);
         }
 
     }
@@ -70,7 +70,7 @@ public class MoodAnalyserTest {
         try {
             MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser();
         } catch (MoodAnalyserException e) {
-            Assert.assertEquals(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD,e.type);
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD, e.type);
         }
 
     }
