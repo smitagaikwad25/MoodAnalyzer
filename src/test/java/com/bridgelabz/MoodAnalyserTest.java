b69a513e9685621b3da5bc6cf02190d89a37042a
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-10 17:18:43 +0530

    Add : TC4.2 improper class

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -54,4 +54,14 @@ public class MoodAnalyserTest {
         MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser();
         Assert.assertEquals(new MoodAnalyser("I am in happy mood"), moodAnalyser);
     }
+
+    @Test
+    public void givenMoodAnalyserClass_whenNotProper_ReturnMoodAnalyserException() {
+        try {
+            MoodAnalyser moodAnalyser = MoodAnalyserFactory.createMoodAnalyser();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS,e.type);
+        }
+
+    }
 }
