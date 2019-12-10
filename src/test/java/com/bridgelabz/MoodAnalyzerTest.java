8eac8b375d194f89761f453dc5f720d7213f32bb
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-10 11:56:55 +0530

    Add :TC2.1 given null mood should return happy

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,20 +4,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
+
     @Test
-    public void givenMessage_WhenSad_Shoud_ReturnSad()
-    {
+    public void givenMessage_WhenSad_Should_ReturnSad(){
         MoodAnalyzer analyzer = new MoodAnalyzer("this Sad is message");
         String mood = analyzer.analyseMood();
         Assert.assertEquals("Sad",mood);
     }
 
     @Test
-    public void givenMessage_WhenAny_ShoudReturnHappy()
-    {
+    public void givenMessage_WhenAny_ShouldReturnHappy(){
         MoodAnalyzer analyzer = new MoodAnalyzer("I am in any mood");
         String mood = analyzer.analyseMood();
         Assert.assertEquals("Happy",mood);
 
+    }
+
+    @Test
+    public void givenMessage_WhenNull_ShouldReturnHappy(){
+            MoodAnalyzer analyzer = new MoodAnalyzer("Null");
+            String  mood = analyzer.analyseMood();
+            Assert.assertEquals("Happy",mood);
     }
 }
