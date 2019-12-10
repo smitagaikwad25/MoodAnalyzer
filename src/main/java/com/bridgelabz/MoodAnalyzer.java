ee7a531a32518478b7512fab803df66f3eb75ba7
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-10 10:00:04 +0530

    Add : Code for any mood return happy

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,10 +1,11 @@
-package com.birdgelabz;
+package com.bridgelabz;
 
 public class MoodAnalyzer {
     public String analyseMood(String s)
     {
         if ( s.contains("Sad"))
             return "Sad";
+        else
             return "Happy";
     }
 }
