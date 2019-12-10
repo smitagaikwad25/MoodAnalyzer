af1b00eee69ebfb6b2b17733a55b48e0ba161335
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-10 18:54:53 +0530

    Add : create param constructor

--- src/main/java/com/bridgelabz/MoodAnalyserFactory.java
+++ src/main/java/com/bridgelabz/MoodAnalyserFactory.java
@@ -23,4 +23,25 @@ public class MoodAnalyserFactory {
         }
         return null;
     }
+
+    public static MoodAnalyser createMoodAnalyser(String message) throws MoodAnalyserException {
+        try {
+            Class<?> moodAnalyserClass = Class.forName("com.bridgelabz.MoodAnalyser");
+            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor(String.class);
+            Object moodObject = moodConstructor.newInstance(message);
+            return (MoodAnalyser) moodObject;
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS, "please Enter Proper Class Name");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD, "please Enter Proper Method Name");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
